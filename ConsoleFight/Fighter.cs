855850b83e71015e83a1458e67df4428b26e6102
BuildTools <unconfigured@null.spigotmc.org>
2017-10-20 15:01:17 -0400

    Added the files we were given and the Visual Studio project

---
+++ ConsoleFight/Fighter.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hunter_X_Hunter_part_2
+{
+    class Fighter
+    {
+        //define all the class variables first these should be private
+        private string name;
+        private int health;
+        private int atk;
+        private int damL;
+        private int damH;
+        private int def;
+        private int speed;
+        private int counterL;
+        private int counterH;
+
+        //now properties which allow us to safely manipulate our variables
+        //properties have getters and setters
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Health
+        {
+            get { return health; }
+            set { health = value; }
+        }
+
+        public int Atk
+        {
+            get { return atk; }
+            set { atk = value; }
+        }
+
+        public int DamL
+        {
+            get { return damL; }
+            set { damL = value; }
+        }
+
+        public int DamH
+        {
+            get { return damH; }
+            set { damH = value; }
+        }
+
+        public int Def
+        {
+            get { return def; }
+            set { def = value; }
+        }
+
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        public int CounterL
+        {
+            get { return counterL; }
+            set { counterL = value; }
+        }
+
+        public int CounterH
+        {
+            get { return counterH; }
+            set { counterH = value; }
+        }
+
+
+
+        // add our constructor
+        public Fighter(string fname)
+        {
+            //set up the fighters based on their name
+            if (fname == "Gon")
+            {
+                Name = "Gon";
+                Atk = 5;
+                DamL = 1;
+                DamH = 4;
+                Def = 5;
+                Speed = 10;
+                Health = 10;
+                CounterL = 3;
+                CounterH = 5;
+            }
+
+            else if (fname == "Hanzo")
+            {
+                Name = "Hanzo";
+                Atk = 7;
+                DamL = 1;
+                DamH = 5;
+                Def = 3;
+                Speed = 5;
+                Health = 10;
+                CounterL = 1;
+                CounterH = 2;
+            }
+
+            else if (fname == "Hizoka")
+            {
+                Name = "Hizoka";
+                Atk = 9;
+                DamL = 2;
+                DamH = 6;
+                Def = 6;
+                Speed = 7;
+                Health = 12;
+                CounterL = 1;
+                CounterH = 6;
+            }
+
+            else if (fname == "Tompa")
+            {
+                Name = "Tompa";
+                Atk = 0;
+                DamL = 0;
+                DamH = 0;
+                Def = 0;
+                Speed = 0;
+                Health = 0;
+                CounterL = 0;
+                CounterH = 0;
+            }
+        }
+        //end contstructor
+
+
+
+        //the damage method to reduce health.
+        public void Damage(int damage)
+        {
+            this.health = this.health - damage;
+        }
+    }
+}
---
+++ ConsoleFight/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Hunter_X_Hunter_part_2
+{
+    class Program
+    {
+
+        static Fighter fighter1;
+        static Fighter fighter2;
+        static Random rnd = new Random();
+        static Arena arena = new Arena();
+
+        static void Main(string[] args)
+        {
+            //greet player
+            Console.WriteLine("***  Welcome to the final round of the hunter exam  ***\n");
+            GenerateFighters();
+            Console.WriteLine($"Today's battle will take place between [{fighter1.Name}] and [{fighter2.Name}]\n\n");
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Lets head to the arena\n");
+            Thread.Sleep(1000);
+
+            //Start the battle and receive the winner
+            bool winner = arena.Battle(fighter1, fighter2);
+
+            if (winner == true)
+            {
+                Console.WriteLine("Congratulations you pass the hunter exam!");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("You've been KOed better luck next year");
+                Console.ReadLine();
+            }
+        }
+
+
+
+
+
+        static void GenerateFighters()
+        {
+            //right now the player can only be Gon
+            fighter1 = new Fighter("Gon");
+            string f2name;
+
+            //calculate probability between 1 and 100;
+            int prob = rnd.Next(1, 101);
+            if (prob > 0 && prob <= 5)
+            {
+                f2name = "Tompa";
+            }
+            else if (prob > 5 && prob <= 20)
+            {
+                f2name = "Hizoka";
+            }
+            else
+            {
+                f2name = "Hanzo";
+            }
+
+            //generate comp fighter
+            fighter2 = new Fighter(f2name);
+        }
+    }
+}
